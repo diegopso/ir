b2daa23f84134f35d5f29acf6f78f774b8dfca24
diegopso <diegopso2@gmail.com>
2012-08-07 11:30:51 -0300

    adicionando comparacao de similaridade de perfis de conhecimentos e avaliacoes durante o processo de inferencia de confianca.

--- src/tsweetselements/OpinionCorrelation.java
+++ src/tsweetselements/OpinionCorrelation.java
@@ -4,7 +4,11 @@
  */
 package tsweetselements;
 
+import connection.ModelContent;
 import connection.ModelViewEvaluations;
+import ir.Centroid;
+import ir.Content;
+import java.util.ArrayList;
 import statistics.Statistics;
 
 /**
@@ -13,6 +17,13 @@ import statistics.Statistics;
  */
 public class OpinionCorrelation {
     public static double correlation_between(Integer source_id, Integer sink_id){
+        return Statistics.average(new Double[]{
+            evaluation_similarity(source_id, sink_id), 
+            knowledge_profile_similarity(source_id, sink_id)
+        });
+    }
+    
+    public static double evaluation_similarity(Integer source_id, Integer sink_id){
         ModelViewEvaluations values = ModelViewEvaluations.get_evaluations_lists(source_id, sink_id);
         
         int aSize = values.aValues.size();
@@ -24,5 +35,15 @@ public class OpinionCorrelation {
         values.bValues.toArray(B);
         
         return Statistics.pearson_coeficient(A, B);
+    }
+    
+    public static double knowledge_profile_similarity(Integer source_id, Integer sink_id){
+        ArrayList<ModelContent> aContents = ModelContent.getContents(source_id);
+        ArrayList<ModelContent> bContents = ModelContent.getContents(sink_id);
+        
+        ArrayList<Content> alpha = ModelContent.toContents(aContents);
+        ArrayList<Content> beta = ModelContent.toContents(bContents);
+        
+        return Content.contentSimilarity(Centroid.getCentroid(alpha), Centroid.getCentroid(beta));
     }
 }
